a6e0ebba2d21e32f56dff55b76ee5f08a8c04a71
shoshgr <shoshgrilak@gmail.com>
2023-09-11 23:11:03 +0300

    initial code + welcome functions

---
+++ dotNet5784_2816_0755/stage0/Program8216.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+ using System;
+namespace stage0
+{
+    partial class Program
+    {
+        static void main(string[] args)
+        {
+            welcome8216();
+            welcom0755();
+            Console.ReadKey();
+            
+        }
+        static void welcome8216()
+        {
+            Console.WriteLine("enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0},welcome to my first console application",name);
+        }
+        static partial void welcom0755();
+    }
+}
